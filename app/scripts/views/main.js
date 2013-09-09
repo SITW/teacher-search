2f1f2af281e3899aa09228fbeb260a29c940b218
howardchi <howardchi@fougood.com>
2013-09-09 09:21:26 +0800

    main page view

---
+++ app/scripts/views/main.js
@@ -0,0 +1,24 @@
+/*global define*/
+
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'templates'
+], function ($, _, Backbone, JST) {
+    'use strict';
+
+    var MainView = Backbone.View.extend({
+
+        el: '#teacher_container',
+
+        template: JST['app/scripts/templates/main.ejs'],
+
+        render: function () {
+            $(this.el).html(this.template());
+            return this;
+        }
+    });
+
+    return MainView;
+});
